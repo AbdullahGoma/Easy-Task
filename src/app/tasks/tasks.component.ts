a2a547178ccb643ab46a5060bd95e4ce249e7ba2
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-05-14 05:12:58 +0300

    Add New Task Dialog and Make it closable

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) name!: string;
   @Input({ required: true }) userId!: string;
+  isAddingTask = false;
   tasks = [
     {
       id: 't1',
@@ -46,5 +48,14 @@ export class TasksComponent {
     if (index !== -1) {
       this.tasks.splice(index, 1);
     }
+  }
+
+  onCloseAddTask(isClose: boolean) {
+    this.isAddingTask = isClose;
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+    // this.tasks.push()
   }
 }
