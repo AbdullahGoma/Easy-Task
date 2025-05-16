f33452867d6a935e9ac2c159cd7ce0189670b6c6
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-05-16 18:16:24 +0300

    Using NgModule Instead of Standalone

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -6,13 +6,10 @@ import {
   Output,
   signal,
 } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
